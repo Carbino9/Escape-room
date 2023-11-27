b891b03c9530c1d25a5ef9e1970ae261271b681b
Carbino9 <149244826+Carbino9@users.noreply.github.com>
2023-11-27 18:25:11 +0200

    Delete Source directory

--- Source/Escape_room/Escape_room.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Escape_room.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Escape_room, "Escape_room" );
- 
